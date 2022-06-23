45d9f5e8d54d6e451e1e561e3fde301e76802ffe
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-23 18:37:15 +0300

    image hover

--- index.js
+++ index.js
@@ -2,18 +2,45 @@ let clicking=document.getElementById("b1").addEventListener("click",clickMe);
 function clickMe(){
     alert("Eat healthy");
 }
-
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-		'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
+		'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com'
 	}
 };
 
-fetch('https://low-carb-recipes.p.rapidapi.com/random', options)
+fetch('https://calorieninjas.p.rapidapi.com/v1/nutrition?query=tomato', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
-  
-  
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+// 		'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://calorieninjas.p.rapidapi.com/v1/nutrition?query=tomato', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+// let ingredients=async()=>{
+
+// let response =await fetch('https://calorieninjas.p.rapidapi.com/v1/nutrition?query=tomato', options);
+    
+//     let tomato=await response.json()
+//     console.log(tomato);
+//     let data= document.getElementById("response")
+
+//     tomato.map((tomato))=>{
+//         let column= 
+//     }
+
+
+
+
+
+
+
+// }
