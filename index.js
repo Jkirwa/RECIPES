7e3e55525e7e5eaec850b0fb3f476a0caa2bd3e8
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-23 14:05:51 +0300

    Added my API link

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ function clickMe(){
     alert("Eat healthy");
 }
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
+	}
+};
 
+fetch('https://low-carb-recipes.p.rapidapi.com/random', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
   
   
