751c4e36d2462a9a482773b11b6e4e2769179785
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-24 11:19:13 +0300

    Added logo to html file

--- index.js
+++ index.js
@@ -1,49 +1,34 @@
-let clicking=document.getElementById("b1").addEventListener("click",clickMe);
-function clickMe(){
-    alert("Eat healthy");
-}
+// let clicking=document.getElementById("b1").addEventListener("click",clickMe);
+// function clickMe(){
+//     alert("Eat healthy");
+// }
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a7d1524d8fmsheac5e052cc02b52p11e6c7jsn6d8c2379e7f4',
-		'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
-	}
-};
-let dataCount= document.getElementById("food")
-let result;
-fetch('https://low-carb-recipes.p.rapidapi.com/random', options)
-	.then(response => response.json())
-	.then((response) => {console.log(response)
-        result=response
-        let row= `<ul><li> ${response.name}+' '+${response.name}+' '+${response.cookTime}+' '+${''}</li></ul>`;
-        dataCount.append('Name:'+result.name+'.'+''+''
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'a7d1524d8fmsheac5e052cc02b52p11e6c7jsn6d8c2379e7f4',
+// 		'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
+// 	}
+// };
+// let dataCount= document.getElementById("food")
+// let result;
+// fetch('https://low-carb-recipes.p.rapidapi.com/random', options)
+// 	.then(response => response.json())
+// 	.then((response) => {console.log(response)
+//         result=response
+//         let row= `<ul><li> ${response.name}+' '+${response.name}+' '+${response.cookTime}+' '+${''}</li></ul>`;
+//         dataCount.append('Name:'+result.name+'.'+''+''
         
-        +'Steps:'+result.steps);
-        dataCount.innerHtml+=row
-    }
+//         +'Steps:'+result.steps);
+//         dataCount.innerHtml+=row
+//     }
     
-    )
-	.catch(err => console.error(err));
- let populateData=(result)=>{
-    //let column= `<ul><li> ${response.id}+' '+${response.name}+' '+${response.cookTime}+' '+${''}</li></ul>`;
-    dataCount.innerHtml+=column
+//     )
+// 	.catch(err => console.error(err));
+//  let populateData=(result)=>{
+    
+//     dataCount.innerHtml+=column
   
 
- }
- populateData(result)
-// let diet=async()=>{
-
-// let response =await fetch('https://low-carb-recipes.p.rapidapi.com/random', options);
-    
-//     let recipes=await response.json()
-//     console.log(recipes);
-//     let dataCount= document.getElementById("response")
-
-//     recipes.map((recipe)=>{
-//         let column= `<li>name: ${recipe.id},${recipe.name}, ${recipe.cookTime}, ${recipe.nutrients}</li>`;
-//         dataCount.innerHtml+=column 
-//     });
-//     diet()
-
-// }
+//  }
+//  populateData(result)
