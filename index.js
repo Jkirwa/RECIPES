26968dfacbc276f15979ecde7ca9ae7880eb5a91
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-24 21:16:17 +0300

    finishing up

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ let clicking=document.getElementById("btn").addEventListener("click",clickMe);
 function clickMe(){
     alert("Eat clean and healthy");
 }
+
 function callBack(){
     document.getElementById("text").textContent= "Look no further we have your next keto recipe. "
 }           
-   
-    
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded ');
     console.log(callBack())
 });
+
 
 document.getElementById('message').onclick = function changeContent() {
 
@@ -18,21 +18,19 @@ document.getElementById('message').onclick = function changeContent() {
     document.getElementById('message').style = "color: green";
 }
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'a7d1524d8fmsheac5e052cc02b52p11e6c7jsn6d8c2379e7f4',
-// 		'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
-// 	}
-// };
-// let dataCount= document.getElementById("food")
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a7d1524d8fmsheac5e052cc02b52p11e6c7jsn6d8c2379e7f4',
+		'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
+	}
+};
+let dataCount= document.getElementById("food")
 
-// fetch('https://low-carb-recipes.p.rapidapi.com/random', options)
-// 	.then(response => response.json())
-// 	.then((response) => {console.log(response)
-//         result=response
-//         let row= ` ${response.name}+' '+${response.name}+' '+${response.cookTime}+' '`;
-//         dataCount.append('Name:'+result.name+'.'+'Steps: '+result.steps);
+fetch('https://low-carb-recipes.p.rapidapi.com/random', options)
+	.then(response => response.json())
+	.then((response) => {console.log(response)
         
-//     })
+    dataCount.append('Name:'+response.name+'Steps: '+response.steps);
+    })
 
