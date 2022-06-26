14c8d6c6a93178da0abbc3f15a2b0d8db3452228
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-26 14:17:04 +0300

    renamed home.html to index.html

--- index.js
+++ index.js
@@ -18,19 +18,19 @@ document.getElementById('message').onclick = function changeContent() {
     document.getElementById('message').style = "color: green";
 }
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a7d1524d8fmsheac5e052cc02b52p11e6c7jsn6d8c2379e7f4',
-		'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
-	}
-};
-let dataCount= document.getElementById("food")
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'a7d1524d8fmsheac5e052cc02b52p11e6c7jsn6d8c2379e7f4',
+// 		'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
+// 	}
+// };
+// let dataCount= document.getElementById("food")
 
-fetch('https://low-carb-recipes.p.rapidapi.com/random', options)
-	.then(response => response.json())
-	.then((response) => {console.log(response)
+// fetch('https://low-carb-recipes.p.rapidapi.com/random', options)
+// 	.then(response => response.json())
+// 	.then((response) => {console.log(response)
         
-    dataCount.append('Name:'+response.name+'Steps: '+response.steps);
-    })
+//     dataCount.append('Name:'+response.name+'.'+'Steps: '+response.steps);
+//     })
 
